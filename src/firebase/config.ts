fa4a763349de068518a4d8ec0153595533625a6e
Ankit Kumar Panda (ADI) <ankitpanda.1404@gmail.com>
2025-11-27 17:36:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4090439552-6a786",
+  "appId": "1:1035599991320:web:6e52d8a6d8fe7b7b32296d",
+  "apiKey": "AIzaSyAO9VZHVbyPjVFP0XOcKD9zV1i9HErVECk",
+  "authDomain": "studio-4090439552-6a786.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1035599991320"
+};
